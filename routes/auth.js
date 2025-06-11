5ad8b711333c389d5b31a9da0d485961e8310561
moralesmejia <mejiajosemanuel.abc@gmail.com>
2025-06-11 15:47:12 -0600

    fix: usar node en producción

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import { register, login, profile } from '../controllers/authController.js';
+
 import { verifyToken } from '../middleware/auth.js';
 const router = express.Router();
+const express = require('express');
+const menuCtrl = require('../controllers/authController.js');
+
+
+
+
+router.get('/', menuCtrl.login);
+router.post('/', menuCtrl.profile);
+router.put('/:id', menuCtrl.register);
+router.delete('/:id', menuCtrl.delete);
+
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/profile', verifyToken, profile);
 
 export default router;
+module.exports = router;
