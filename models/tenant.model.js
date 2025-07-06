51fbf75272bc8f8980c0602ebfbabe48f7821393
moralesmejia <mejiajosemanuel.abc@gmail.com>
2025-07-05 18:37:13 -0600

    fix: usar node en producción

--- models/tenant.model.js
+++ models/tenant.model.js
@@ -1,10 +1,13 @@
+// tenant.model.js
 import mongoose from 'mongoose';
 
 const tenantSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
+  descripcion: { type: String },
+  tenantId: { type: String, required: true, unique: true }, // Esto faltaba
   plan: { type: String, enum: ['free', 'premium', 'enterprise'], default: 'free' },
   activo: { type: Boolean, default: true },
   creadoEn: { type: Date, default: Date.now }
 });
 
-export default mongoose.model('Tenant', tenantSchema);
+export const Tenant = mongoose.model('Tenant', tenantSchema);
